f6fadf4bfe38e879ca6257a9bd2bd278a6deb717
Luiz Gustavo Caciatori <luiz.caciatori@rokoit.com.br>
2015-12-19 13:05:32 -0200

    Adicionado interface principal para o projeto

--- Xamarin4HackApp/Xamarin4HackApp/Xamarin4HackApp/App.cs
+++ Xamarin4HackApp/Xamarin4HackApp/Xamarin4HackApp/App.cs
@@ -11,18 +11,20 @@ namespace Xamarin4HackApp
 	{
 		public App ()
 		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+            // The root page of your application
+            //MainPage = new ContentPage {
+            //	Content = new StackLayout {
+            //		VerticalOptions = LayoutOptions.Center,
+            //		Children = {
+            //			new Label {
+            //				XAlign = TextAlignment.Center,
+            //				Text = "Welcome to Xamarin Forms!"
+            //			}
+            //		}
+            //	}
+            //};
+
+            MainPage = new MainPageAbstract();
 		}
 
 		protected override void OnStart ()
---
+++ Xamarin4HackApp/Xamarin4HackApp/Xamarin4HackApp/MainPageAbstract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Xamarin4HackApp
+{
+	public class MainPageAbstract : ContentPage
+	{
+		public MainPageAbstract()
+		{
+			Content = new StackLayout {
+				Children = {
+					new Xamarin.Forms.Label { Text = "INFORMAÇÕES" }
+				}
+			};
+		}
+	}
+}
